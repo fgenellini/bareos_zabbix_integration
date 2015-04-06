3ff05c6ce528fb43d9eb06cd055910aa33dc8c3a
Oleg Palij <o.palij@gmail.com>
2015-04-06 21:25:04 +0300

    rewritten to bareos 'show jobs' and simplified

--- get-bacula-jobs-json.py
+++ get-bacula-jobs-json.py
@@ -1,31 +1,17 @@
 #!/usr/bin/env python
 
 import subprocess
+import locale
+import json
+
 from conf import conf
 
-command="echo quit | timeout %d bconsole -c '%s'" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
-exit_code=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE).wait()
-if exit_code != 0:
-	print file(conf['jobs_cache_file']).read()
-	quit(1)
+encoding = locale.getdefaultlocale()[1]
 
-out=open(conf['jobs_cache_file'],'w')
-out.write('{ "data": [ \n')
-
-command="echo show jobs | timeout %d bconsole -c %s | awk '/^Job/ {sub(\"name=\",\"\",$2); print $2}'" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
+command="echo show jobs | timeout %d bconsole -c %s | grep -i name | cut -d \'\"\' -f 2" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
 proc=subprocess.Popen(command, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 jobs_raw=proc.communicate()[0]
-firstrun=1
-for job in jobs_raw.split('\n'):
-	if job == '': continue
-	if firstrun != 1:
-		out.write(',\n')
-	else:
-		firstrun=0
-	out.write("{ \"{#JOBNAME}\":\"%s\"}" % job)
 
-out.write(' ] }')
-out.close()
+result = dict( {'data': [ { "{#JOBNAME}": job } for job in jobs_raw.decode(encoding).split('\n') if job ]} )
 
-print file(conf['jobs_cache_file']).read()
-
+print(json.JSONEncoder(sort_keys=True, indent=3).encode(result))
