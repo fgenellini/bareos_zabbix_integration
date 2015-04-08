0f4ae79ae0ebee13e2eb9f2bc88aaedad9bf42ad
Oleg Palij <o.palij@gmail.com>
2015-04-08 11:48:13 +0300

    template fix

--- get-bareos-jobs-json.py
+++ get-bareos-jobs-json.py
@@ -9,7 +9,7 @@ from conf import conf
 encoding = locale.getdefaultlocale()[1]
 
 command="echo show jobs | timeout %d bconsole -c %s | grep -i name | cut -d \'\"\' -f 2" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
-proc=subprocess.Popen(command, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+proc=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 jobs_raw=proc.communicate()[0]
 
 result = dict( {'data': [ { "{#JOBNAME}": job } for job in jobs_raw.decode(encoding).split('\n') if job ]} )
